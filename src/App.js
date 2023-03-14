a14a944ddc42ddb843069e6e0d4ba288991eded3
Katrya-Kovalova <katya2441@gmail.com>
2023-03-14 22:11:19 +0200

    added the githur url

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import react from "react";
+import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Katya Kovalyova and is{" "}
+        <a
+          href="https://github.com/Katrya-Kovalova/weather-app-react"
+          target="_blank"
+        >
+          {" "}
+          onen-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
